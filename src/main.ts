e9914e7a30c810bd39520a7acf2233dd7ff447b4
Moses <obikamoses@gmail.com>
2021-11-17 11:34:53 +0100

    allow cors fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('The My Art Stock')
     .setDescription('The My Art Stock API description')
@@ -18,9 +18,21 @@ async function bootstrap() {
   //const { httpAdapter } = app.get(HttpAdapterHost);
   //app.useGlobalFilters(new CustomExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
+  /* app.enableCors({
     origin: /https?:\/\/(([^/]+\.)?example\.com)$/i,
     //credentials: true,
+  }); */
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
   });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(process.env.PORT || 3000);
